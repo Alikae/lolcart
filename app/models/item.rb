4e0453ae400d83036f2e082b1fb9f7a49d8aba75
Alikae <alikae@laposte.net>
2018-03-05 23:28:31 +0100

    create models Cart+half Order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+    has_many :carts
+    has_many :orders
     validates :title, :description, :prices, :image_url, presence: true
     validates :description, length: { maximum: 140 }
 

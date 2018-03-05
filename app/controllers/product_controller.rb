c0eadb1b25b1446b86e4b638d5b273ee4c3a2f17
lobamos <namosdjoro@gmail.com>
2018-03-05 17:11:22 +0100

    crud

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -32,7 +32,7 @@ class ProductController < ApplicationController
     def destroy
       @article = Article.find params[:id]
       @article.destroy
-      redirect_to articles_path 
+      redirect_to articles_path
     end
     end
 

7984825dea6366998b7280f62eb848bfa83d76eb
francoispetit <francois.petit@outlook.com>
2018-03-06 12:19:33 +0100

    corrigé validates price

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
     has_many :carts
     has_many :orders
-    validates :title, :description, :prices, :image_url, presence: true
+    validates :title, :description, :price, :image_url, presence: true
     validates :description, length: { maximum: 140 }
 
 end

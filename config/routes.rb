c14546389dc2b3848f10533dfe79f913654cb75c
francoispetit <francois.petit@outlook.com>
2018-03-07 17:41:25 +0100

    ajout au panier depuis index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :items
   resources :carts
   post 'items/show/:id', to: 'carts#add_item', as: :add_item
+	post 'root/:id', to: 'carts#add_item', as: :add_item_from_index
   delete 'carts/show/:id', to: 'carts#remove_item', as: :remove_item
   get 'items/index'
   get 'items/show'

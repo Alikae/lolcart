2b4d6b2fb7d74ad9d2c029a00fa33996b8396147
francoispetit <francois.petit@outlook.com>
2018-03-07 12:48:05 +0100

    work in progress

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,6 +9,11 @@ class CartsController < ApplicationController
     @cart = Cart.create(cart_params)
   end
 
+  def add_item(item_id)
+    @cart = current_user.cart
+    @cart << Item.find(item_id)
+  end
+
   def show
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   has_one :cart
   has_many :items, through: :cart
 
-  has_one :order
+  has_many :orders
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def after_create
+    
+  end
+
 end
